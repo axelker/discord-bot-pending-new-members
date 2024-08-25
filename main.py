6b6458aa6a79f6ed8a6260c0f8e242e5eecc23ea
axelker <axelker@outlook.fr>
2024-08-25 23:33:44 +0200

    feat: Imporve error send messages

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
-intents = discord.Intents.default()
-intents.members = True
+intents = discord.Intents.all() 
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+pending_role_name = 'pending'
 
 @bot.event
 async def on_ready():
@@ -16,7 +16,7 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    pending_role = discord.utils.get(member.guild.roles, name='pending')
+    pending_role = discord.utils.get(member.guild.roles, name=pending_role_name)
     
     if pending_role:
         await member.add_roles(pending_role)
@@ -25,15 +25,19 @@ async def on_member_join(member):
 @bot.command()
 @commands.has_permissions(manage_roles=True)
 async def approve(ctx, member: discord.Member):
-    pending_role = discord.utils.get(ctx.guild.roles, name='pending')
+    base_role_name = 'user'
+    pending_role = discord.utils.get(ctx.guild.roles, name=pending_role_name)
+    if not pending_role:
+        await ctx.send(f"Le rôle {pending_role_name} n'existe pas.")
+        return
     if pending_role in member.roles:
         await member.remove_roles(pending_role)
-        await member.add_roles('user')
-        await ctx.send(f"{member.mention} a rejoint le serveur!")
+        await member.add_roles(base_role_name)
+        await ctx.send(f"{member.mention} a rejoint le serveur en tant que {base_role_name}!")
     else:
-        await ctx.send(f"{member.mention} est déja présent sur le serveur.")
+        await ctx.send(f"{member.mention} est déja présent sur le serveur et ne possède pas le role {pending_role_name}.")
 
-# Gérer l'erreur si un utilisateur sans les bonnes permissions essaie d'exécuter la commande
+# Manage error for user not have the permission
 @approve.error
 async def approve_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
